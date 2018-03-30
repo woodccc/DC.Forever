ebe406a23611b0a75bcb309563d3aec81383bad3
chenlong <woodccc@foxmail.com>
2018-03-30 14:09:02 +0800

    用 koa 替换 express

--- back-end/app.js
+++ back-end/app.js
@@ -1,65 +1,56 @@
-var express = require('express');
-var path = require('path');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+const Koa = require('koa')
+const app = new Koa()
+const views = require('koa-views')
+const json = require('koa-json')
+const onerror = require('koa-onerror')
+const bodyparser = require('koa-bodyparser')
+const logger = require('koa-logger')
+const mongoose = require('mongoose')
+const cors = require('koa-cors')
 
-var index = require('./routes/index');
-var weight = require('./routes/weight');
-
-var app = express();
-
-var allowCrossDomain = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-    res.header('Access-Control-Allow-Credentials','true');
-    next();
-};
-app.use(allowCrossDomain);
-
-app.use(logger('dev'));
-app.use(bodyParser.urlencoded({ extended: false}));
-app.use(bodyParser.json());
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-//router ==========================================
-app.use('/weight',weight);
-app.use('/', index);
-//router ==========================================
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
+const weight = require('./routes/weight')
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+onerror(app)
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-var mongoose = require('mongoose');
-
+// 连接数据库
 mongoose.connect('mongodb://localhost/TWood')     //连接本地数据库blog
-
-var db = mongoose.connection;
-
+const db = mongoose.connection
 // 连接成功
 db.on('open', function(){
-    console.log('MongoDB Connection Successed');
+	console.log('MongoDB Connection Successed');
 });
 // 连接失败
 db.on('error', function(){
-    console.log('MongoDB Connection Error');
+	console.log('MongoDB Connection Error');
 });
 
-module.exports = app;
+// middlewares
+app.use(cors())
+app.use(bodyparser({
+  enableTypes:['json', 'form', 'text']
+}))
+app.use(json())
+app.use(logger())
+app.use(require('koa-static')(__dirname + '/public'))
+app.use(views(__dirname + '/views', {
+  extension: 'pug'
+}))
+
+// logger
+app.use(async (ctx, next) => {
+  const start = new Date()
+  await next()
+  const ms = new Date() - start
+  console.log(`${ctx.method} ${ctx.url} - ${ms}ms`)
+})
+
+// routes
+app.use(weight.routes(), weight.allowedMethods())
+
+// error-handling
+app.on('error', (err, ctx) => {
+  console.error('server error', err, ctx)
+});
+
+module.exports = app
--- back-end/routes/weight.js
+++ back-end/routes/weight.js
@@ -1,64 +1,35 @@
-const express = require('express');
-const router = express.Router();
+const router = require('koa-router')()
 
 const Weight = require("../models/weight");
 
-router.get("/", (req, res) => {
-    Weight.find({})
-        .then(weight => {
-            res.json(weight);
-        })
-        .catch(err => {
-            console.log(2);
-            res.json(err);
-        });
-});
-//
-// // 通过ObjectId查询单个英雄信息路由
-// router.get("/hero/:id", (req, res) => {
-//     Hero.findById(req.params.id)
-//         .then(hero => {
-//             res.json(hero);
-//         })
-//         .catch(err => {
-//             res.json(err);
-//         });
-// });
-//
-router.post("/", (req, res) => {
-    Weight.create(req.body, (err, weight) => {
-        if (err) {
-            res.json(err);
-        } else {
-            res.json(weight);
-        }
-    });
+router.prefix('/weight')
+
+router.get('/', async (ctx, next) => {
+	ctx.body = await Weight.find({})
+})
+
+router.post("/", async(ctx, next) => {
+	ctx.body = await Weight.create(ctx.request.body);
 });
 
-router.put("/:id", (req, res) => {
-    Weight.findOneAndUpdate(
-        { _id: req.params.id },
+router.put("/:id", async(ctx, next) => {
+		ctx.body = await Weight.findOneAndUpdate(
+        { _id: ctx.params.id },
         {
             $set: {
-                date: req.body.date,
-                tag: req.body.tag,
-                weight: req.body.weight
+                date: ctx.request.body.date,
+                tag: ctx.request.body.tag,
+                weight: ctx.request.body.weight
             }
         },
         {
             new: true
         }
     )
-        .then(weight => res.json(weight))
-        .catch(err => res.json(err));
 });
 
-router.delete("/:id", (req, res) => {
-    Weight.findOneAndRemove({
-        _id: req.params.id
-    })
-        .then(weight => res.send('删除成功'))
-        .catch(err => res.json(err));
+router.delete("/:id", async(ctx, next) => {
+			ctx.body = await Weight.findOneAndRemove({_id: ctx.params.id})
 });
 
 module.exports = router;
