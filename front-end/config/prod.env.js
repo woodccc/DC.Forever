b5fd459e26cfc5e81f69250a4e1349bc9f3a67d7
chenlong <woodccc@foxmail.com>
2018-04-16 18:20:17 +0800

    修改后端 ip。

--- front-end/config/prod.env.js
+++ front-end/config/prod.env.js
@@ -4,5 +4,5 @@ module.exports = {
   // URL_PREFIX: 'http://192.168.43.193:3000/' //p9
   // URL_PREFIX: 'http://localhost:3000/'
   // URL_PREFIX: 'http://192.168.1.4:3000/' //home
-  URL_PREFIX: 'http://192.168.1.5:3000/' //5
+  URL_PREFIX: 'http://192.168.1.3:3000/' //3
 }
